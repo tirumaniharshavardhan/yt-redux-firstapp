5c148de0e31bdcfcf4ab128a466786607109ac90
tirumaniharshavardhan <tirumaniharshavardhan@gmail.com>
2024-11-03 23:07:01 +0530

    added remove from cart feature

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,34 @@
+import { useSelector } from "react-redux";
+import { Card } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+
 const Cart = () => {
-    return(
-        <h2>Cart</h2>
+    const products = useSelector(state => state.cart)
+
+    const cards = products.map(product => {
+        return (
+            <div className="col-md-12" style={{ marginBottom: '10px', display: 'flex', justifyContent: 'space-around' }}>
+                <Card style={{ width: '18rem' }} key={product.id} className="h-100">
+                    <div className="text-center">
+                        <Card.Img variant="top" src={product.image} style={{ width: '100px', height: '100px' }} />
+                    </div>
+                    <Card.Body>
+                        <Card.Title>{product.title}</Card.Title>
+                        <Card.Text>
+                            INR: {product.price}
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        <Button variant="danger">Remove item</Button>
+                    </Card.Footer>
+                </Card>
+            </div>
+        )
+    })
+    return (
+        <div>
+            {cards}
+        </div>
     )
 }
 
