1cedecf0cd678a1fdfe97773b4a6b9837a7c9412
tirumaniharshavardhan <tirumaniharshavardhan@gmail.com>
2024-11-03 21:06:36 +0530

    added different components and routing

---
+++ src/components/Cart.js
@@ -0,0 +1,7 @@
+const Cart = () => {
+    return(
+        <h2>Cart</h2>
+    )
+}
+
+export default Cart;
---
+++ src/components/Dashboard.js
@@ -0,0 +1,9 @@
+import Product from './Product.js'
+
+const Dashboard = () => {
+    return(
+        <Product/>
+    )
+}
+
+export default Dashboard;
